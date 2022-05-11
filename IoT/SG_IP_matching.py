5b46d8ffedf524ae2878c9215b07aa02ee20a121
thangwk <49297115+thangwk@users.noreply.github.com>
2022-05-11 10:42:44 +0800

    added 2 more functions IP_address_filter.py & SG_IP_matching.py

---
+++ IoT/SG_IP_matching.py
@@ -0,0 +1,20 @@
+# this function is to evaluate if IPs are from SG
+import ipaddress
+import pandas as pd
+
+def match_SG_IP(df, output_filename):
+    # load PoC-monitored-APIxlsx file
+    SG_IP = pd.read_excel("PoC-monitored-APIxlsx.xlsx")
+
+    is_SG_IP = []
+    for i in df['SrcIP']:
+        for ip in range(0, len(SG_IP['Monitored Subnets'])-2):
+            if ip_address.ip_address(i) in ipaddress.ip_network(SG_IP['Monitored Subnets'][ip]):
+                val = SG_IP['As Name'][ip]
+                break
+            else:
+                val = 'No'
+        is_SG_IP.append(val)
+    df['As Name'] = is_SG_IP
+    df.to_csv("check_IP_address_" + output_filename + ".csv", index=False)
+    return df 
