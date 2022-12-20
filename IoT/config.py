76aee9c7dbef247a63fd4147cd4558889f1583e7
thangwk <49297115+thangwk@users.noreply.github.com>
2022-12-20 17:35:38 +0800

    Delete config.py

--- IoT/config.py
+++
@@ -1,7 +0,0 @@
-#for OpenSearch
-Opensearch_API_key = '{"username":"csa_user", "password":"Qihys-CSA-2021"}'
-#shodan
-shodan_api_key ="8FBW2mwRabHMFc8w4OUSRFACAP2qFgRR"
-
-#for virustotal log in
-api_key = '1d0520ee70aca1530e9665d6d3be394915bdb0f4e4b6cc961e1ddc8d5c5c1b58'
